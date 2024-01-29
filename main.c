364ab7de3366a655344864165c24f31f94fe6489
reaulou <0juantombeng@gmail.com>
2024-01-30 01:04:07 +0700

    added map grid and player rotation

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "glut.h"
+#include<math.h>
+#define PI 3.14159265
 
-float px, py;
+float px, py, pdx, pdy, pa;
 
 void drawPlayer(){
     glColor3f(1,1,0);
@@ -10,27 +12,63 @@ void drawPlayer(){
     glBegin(GL_POINTS);
     glVertex2i(px, py);
     glEnd();
+
+    glLineWidth(3);
+    glBegin(GL_LINES);
+    glVertex2i(px, py);
+    glVertex2i(px+pdx*5, py+pdy*5);
+    glEnd();
+}
+
+int mapX=8, mapY=8, mapS=64;
+int map[]=
+{
+    1,1,1,1,1,1,1,1,
+    1,0,0,0,1,0,0,1,
+    1,1,1,0,1,1,0,1,
+    1,0,0,0,0,0,0,1,
+    1,0,0,1,1,1,0,1,
+    1,1,0,0,0,0,0,1,
+    1,0,0,0,1,0,0,1,
+    1,1,1,1,1,1,1,1,
+};
+
+void drawMap2D(){
+    int x, y, xo, yo;
+    for(y=0; y<mapY;y++){
+        for(x=0; x<mapX; x++){
+            if(map[y*mapX+x] == 1){glColor3f(1,1,1);}else{glColor3f(0,0,0);}
+            xo=x*mapS; yo=y*mapS;
+            glBegin(GL_QUADS);
+            glVertex2i(xo+1, yo+1);
+            glVertex2i(xo+1, yo+mapS-1);
+            glVertex2i(xo+mapS-1, yo+mapS-1);
+            glVertex2i(xo+mapS-1, yo+1);
+            glEnd();
+        }
+    }
 }
 
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+    drawMap2D();
     drawPlayer();
     glutSwapBuffers();
 }
 
 void buttons(unsigned char key, int x, int y){
-    if(key=='a'){px-=5;}
-    if(key=='d'){px+=5;}
-    if(key=='w'){py-=5;}
-    if(key=='s'){py+=5;}
+    if(key=='a'){pa-=0.1; if(pa<0){pa+=2*PI;} pdx=cos(pa)*5, pdy=sin(pa)*5;}
+    if(key=='d'){pa+=0.1; if(pa>2*PI){pa-=2*PI;} pdx=cos(pa)*5, pdy=sin(pa)*5;}
+    if(key=='w'){px+=pdx;py+=pdy;}
+    if(key=='s'){px-=pdx;py-=pdy;}
     glutPostRedisplay();
 }
 
 void init(){
     glClearColor(0.3,0.3,0.3,0);
     gluOrtho2D(0,1024,512,0);
-    px=300; py=300;
+    px=300; py=300; pdx=cos(pa)*5, pdy=sin(pa)*5;
 }
 
 int main(int argc, char** argv)
