022b5bd215697b6e7280a79af4818134b25b572a
reaulou <0juantombeng@gmail.com>
2024-01-30 00:47:14 +0700

    added player

--- main.c
+++ main.c
@@ -2,16 +2,35 @@
 #include<stdlib.h>
 #include "glut.h"
 
+float px, py;
+
+void drawPlayer(){
+    glColor3f(1,1,0);
+    glPointSize(8);
+    glBegin(GL_POINTS);
+    glVertex2i(px, py);
+    glEnd();
+}
+
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-
+    drawPlayer();
     glutSwapBuffers();
+}
+
+void buttons(unsigned char key, int x, int y){
+    if(key=='a'){px-=5;}
+    if(key=='d'){px+=5;}
+    if(key=='w'){py-=5;}
+    if(key=='s'){py+=5;}
+    glutPostRedisplay();
 }
 
 void init(){
     glClearColor(0.3,0.3,0.3,0);
     gluOrtho2D(0,1024,512,0);
+    px=300; py=300;
 }
 
 int main(int argc, char** argv)
@@ -22,6 +41,7 @@ int main(int argc, char** argv)
     glutCreateWindow("Raycaster");
     init();
     glutDisplayFunc(display);
+    glutKeyboardFunc(buttons);
     glutMainLoop();
     return 0;
 }
